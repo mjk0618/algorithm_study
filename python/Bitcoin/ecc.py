03fbf1bc4c6e8f5f9d6990fa5256540cfb54c769
MinJae Kang <kminjae618@gmail.com>
2022-09-10 02:55:17 +0900

    [Edit] 3_ecc.ipynb

--- python/Bitcoin/ecc.py
+++ python/Bitcoin/ecc.py
@@ -132,6 +132,16 @@ class Point:
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
 
+    '''
+    # 곱하는 값이 커지면 연산 시간이 오래 걸림
+    def __rmul__(self, coefficient):
+        product = self.__class__(None, None, self.a, self.b)
+        for _ in range(coefficient):
+            product += self
+        return product
+    '''
+
+    # 이진수 전개(binary expansion) 방법으로 위 문제 해결
     def __rmul__(self, coefficient):
         coef = coefficient
         current = self
@@ -142,7 +152,7 @@ class Point:
             current += current
             coef >>= 1
         return result
-
+    
 class ECCTest(TestCase):
 
     def test_add(self):
@@ -170,3 +180,97 @@ class ECCTest(TestCase):
             p3 = Point(x3, y3, a, b)
             self.assertEqual(p1 + p2, p3)
 
+
+
+P = 2 ** 256 - 2 ** 32 - 977
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num = num, prime = P)
+
+    def __repr__(self):
+        return f'{self.num:x}'.zfill(64)
+
+A = 0
+B = 7
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+
+class S256Point(Point):
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x = S256Field(x), y = S256Field(y), a = a, b = b)
+        else:
+            super().__init__(x = x, y = y, a = a, b = b)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'S256Point(infinity)'
+        else:
+            return f'S256Point({self.x}, {self.y})'
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+)
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+    
+    def __repr__(self):
+        return f'Signature({self.r:x}, {self.s:x})'
+
+class PrivateKey:
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return f'{self.secret:x}'.zfill(64)
+
+    def sign(self, z):
+        k = self.deterministic_k(z)
+        r = (k * G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N/2:
+            s = N - s
+        return Signature(r, s)
+    
+    def deterministic_k(self, z):
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).giest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
