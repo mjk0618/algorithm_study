e4106e91211f80a1822f7c2bf4ab0da081fd208e
MinJae Kang <kminjae618@gmail.com>
2023-01-18 16:41:54 +0900

    [Add] Various Files

--- python/coding_test/003.py
+++ python/coding_test/003.py
@@ -3,3 +3,25 @@
 ## 내풀이
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
+for i in range(m):
+    i, j = map(int, input().split())
+    print(sum(numbers[i - 1: j]))
+
+# 위와 같이 구현하면 시간초과가 발생한다.
+# 따라서 구간합 배열을 미리 만들어놓아야 한다.
+
+## 내풀이 수정
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+prefix_sum = [0]
+temp = 0
+
+for i in numbers:
+    temp = temp + i
+    prefix_sum.append(temp)
+
+for i in range(m):
+    i, j = map(int, input().split())
+    print(prefix_sum[j] - prefix_sum[i - 1])
